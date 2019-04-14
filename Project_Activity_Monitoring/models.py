1d7d6238b06884e7b804b0ae069208fafa28f62d
b0ran0v <rustam.bagituly@gmail.com>
2019-04-14 23:16:57 +0600

    Added Task model

--- Project_Activity_Monitoring/models.py
+++ Project_Activity_Monitoring/models.py
@@ -11,3 +11,13 @@ class Project(models.Model):
     def __str__(self):
         return self.title
 
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    is_finished = models.BooleanField(default=False)
+    date_assigned = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
