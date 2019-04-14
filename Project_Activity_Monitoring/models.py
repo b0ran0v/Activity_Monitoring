c460a265edb1646a6ea853329fb3c515a9129cb7
b0ran0v <rustam.bagituly@gmail.com>
2019-04-14 23:13:34 +0600

    Added Project model

--- Project_Activity_Monitoring/models.py
+++ Project_Activity_Monitoring/models.py
@@ -2,3 +2,12 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    requirements = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
