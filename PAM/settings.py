d167b9d0594baeb766732deefa1424aa1e73cc7a
b0ran0v <rustam.bagituly@gmail.com>
2019-04-15 00:20:10 +0600

    Added field permission settings

--- PAM/settings.py
+++ PAM/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',  # keep this bad boy
+    'field_permissions.backends.InstancePermissionBackend',
+]
