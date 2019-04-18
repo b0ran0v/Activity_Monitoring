dae96aae1c56d64c8e4ae419bd67e2f5d9f9a7df
b0ran0v <rustam.bagituly@gmail.com>
2019-04-18 22:45:50 +0600

    Created base html, created project detail html, url and view

--- Project_Activity_Monitoring/views.py
+++ Project_Activity_Monitoring/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 
 def project_list(request):
     projects = Project.objects.order_by('date_created')
     return render(request, 'Project_Management/project_list.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'Project_Management/project_detail.html', {'project': project})
 
