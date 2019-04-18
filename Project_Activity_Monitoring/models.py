57e17d126c9cc2c3eb0fd45656da0cb1392008eb
b0ran0v <rustam.bagituly@gmail.com>
2019-04-18 21:51:34 +0600

    Added project list url, view and template

--- Project_Activity_Monitoring/models.py
+++ Project_Activity_Monitoring/models.py
@@ -1,23 +1,35 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from field_permissions.models import FieldPermissionModelMixin
+from django.contrib.auth.models import AbstractUser
 
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    requirements = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+    project_manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                        blank=True, null=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_finished = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
 
-class Task(models.Model):
+class Task(FieldPermissionModelMixin, models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    developer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                  blank=True, null=True)
     title = models.CharField(max_length=50)
     description = models.TextField()
-    is_finished = models.BooleanField(default=False)
+    is_finished = models.BooleanField(settings.AUTHENTICATION_BACKENDS, default=False)
     date_assigned = models.DateField(blank=True, null=True)
+    deadline = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        permissions = (('can_change_task_status', 'Can change task status'),)
--- Project_Activity_Monitoring/views.py
+++ Project_Activity_Monitoring/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+def project_list(request):
+    projects = Project.objects.order_by('date_created')
+    return render(request, 'Project_Management/project_list.html', {'projects': projects})
+
